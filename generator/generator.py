b111600698fc58799796f6881fe350de2f544798
binoll <onionlsfa@gmail.com>
2024-01-24 13:10:33 +0300

    refactor: upd generator.py

--- generator/generator.py
+++ generator/generator.py
@@ -6,14 +6,11 @@ url = 'http://localhost/add'
 
 
 def generate_title():
-	return lorem.sentence()
+    return lorem.sentence()
 
 
 for i in range(GENERATION_COUNT):
-	payload = {
-		'title': generate_title()
-	}
-	response = requests.post(url, data=payload)
-	
-	if response.status_code != 200:
-		print(f"Error sending POST request: {response.status_code}")
+    payload = {
+        'title': generate_title()
+    }
+    response = requests.post(url, data=payload)
